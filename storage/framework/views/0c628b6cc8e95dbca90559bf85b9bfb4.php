ded7709df9d7353fceeae687e5cf2e372c33e1eb
Reky13 <133196515+Reky13@users.noreply.github.com>
2023-07-13 21:08:17 +0700

    Add files via upload

---
+++ storage/framework/views/0c628b6cc8e95dbca90559bf85b9bfb4.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/sass/app.scss', 'resources/js/app.js']); ?>
+</head>
+<body style="background-color: #3700ff;">
+    <div id="app">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div style="margin-top:130px;" class="col-md-5">
+                    <div class="card">
+                        <i style="color:rgb(255, 255, 255); margin-left:250px; font-size:50px;" class="bi-hexagon-fill me-6"></i>
+                        <center><h1 style="font-size: 30px;">Employee Data Master</h1></center>
+                        <div class="card-body">
+                            <form method="POST" action="<?php echo e(route('login')); ?>">
+                                <?php echo csrf_field(); ?>
+
+                                <div class="row mb-3">
+                                    <label for="email" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Email')); ?></label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="email" value="<?php echo e(old('email')); ?>" placeholder="Enter Your Email" required autocomplete="email" autofocus>
+
+                                        <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong><?php echo e($message); ?></strong>
+                                            </span>
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="password" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Password')); ?></label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="password" placeholder="Enter Your Password" required autocomplete="current-password">
+
+                                        <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong><?php echo e($message); ?></strong>
+                                            </span>
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
+
+                                            <label class="form-check-label" for="remember">
+                                                <?php echo e(__('Remember Me')); ?>
+
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary"><i class="bi bi-box-arrow-right"></i>
+                                            <?php echo e(__('Log In')); ?>
+
+                                        </button>
+
+                                        <?php if(Route::has('password.request')): ?>
+                                            <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
+                                                <?php echo e(__('Forgot Your Password?')); ?>
+
+                                            </a>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <main class="py-4">
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\Users\Asus\your-project-aeon\resources\views/auth/login.blade.php ENDPATH**/ ?>
